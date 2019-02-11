b20a8e1b787b9c346d7024dc266ce5673913cd90
Alexey Malov <alexey.malov@ispringsolutions.com>
2019-02-11 17:41:05 +0300

    Реализован поиск верхней границы при двоичном поиске

--- simple/binary-search-tests.cpp
+++ simple/binary-search-tests.cpp
@@ -1,4 +1,5 @@
 #include "pch.h"
+
 #include "binary-search.h"
 
 using namespace std;
@@ -14,8 +15,18 @@ TEST_CASE("Lower bound is an index of an item that is not less than searched")
 	CHECK(BinarySearch({ 1, 3, 3, 3, 3 }, 3) == Range(1, 5));
 	CHECK(BinarySearch({ 3, 3, 3, 3, 3, 3 }, 3) == Range(0, 6));
 	CHECK(BinarySearch({ 1, 2, 3, 4, 4 }, 4) == Range(3, 5));
-	CHECK(BinarySearch({ 1, 2, 3, 4, 5, 6}, 6) == Range(5, 6));
+	CHECK(BinarySearch({ 1, 2, 3, 4, 5, 6 }, 6) == Range(5, 6));
 
 	CHECK(BinarySearch({ 1, 2, 3, 4, 5, 6 }, 7) == Range(6, 6));
 	CHECK(BinarySearch({ 1, 2, 3, 4, 5, 6 }, 10) == Range(6, 6));
+}
+
+TEST_CASE("Upper bound is an index of an item that is greater than the searched")
+{
+	CHECK(BinarySearch({ 1, 3, 3, 3, 3 }, 1) == Range(0, 1));
+	CHECK(BinarySearch({ 1, 3, 3, 5, 8, 8 }, 8) == Range(4, 6));
+	CHECK(BinarySearch({ 1, 3, 3, 5, 8, 8 }, 9) == Range(6, 6));
+	CHECK(BinarySearch({ 1, 3, 4, 5, 8, 8 }, 5) == Range(3, 4));
+	CHECK(BinarySearch({ 1, 3, 3, 5, 8, 8 }, 4) == Range(3, 3));
+	CHECK(BinarySearch({ 1, 3, 3, 5, 8, 8 }, 0) == Range(0, 0));
 }
--- simple/binary-search.cpp
+++ simple/binary-search.cpp
@@ -6,8 +6,7 @@ using namespace std;
 
 size_t FindLowerBound(const vector<int>& items, size_t first, size_t last, int value)
 {
-	ptrdiff_t count = last - first;
-
+	size_t count = last - first;
 	while (count > 0) // »щем пока диапазон не пуст
 	{
 		auto step = count / 2;
@@ -28,10 +27,29 @@ size_t FindLowerBound(const vector<int>& items, size_t first, size_t last, int v
 	return first;
 }
 
+size_t FindUpperBound(const vector<int>& items, size_t first, size_t last, int value)
+{
+	size_t count = last - first;
+	while (count > 0) // »щем пока диапазон не пуст
+	{
+		auto step = count / 2;
+		auto mid = first + step;
+		if (value < items[mid]) // искомый элемент слева от mid?
+		{	// ќтбрасываем правую половину
+			count = step;
+		}
+		else // »скомый элемент в позиции mid или правее
+		{	// ќтбрасываем левую половину
+			first = mid + 1;
+			count -= step + 1;
+		}
+	}
+	return first;
+}
+
 Range BinarySearch(const vector<int>& items, int value)
 {
-	size_t last = items.size();
-	size_t first = FindLowerBound(items, 0, last, value);
-
+	size_t first = FindLowerBound(items, 0, items.size(), value);
+	size_t last = FindUpperBound(items, first, items.size(), value);
 	return { first, last };
 }
